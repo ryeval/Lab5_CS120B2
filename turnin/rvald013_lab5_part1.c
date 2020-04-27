71decfe62b2cb8011e6eb90118a2e88e8c00303e
Ryan Valdeavil Valdeavilla <atomicfailure32@gmail.com>
2020-04-26 19:50:44 -0700

    finished part 1

--- turnin/rvald013_lab5_part1.c
+++ turnin/rvald013_lab5_part1.c
@@ -1,7 +1,7 @@
 /*	Author: Ryan Valdeavilla
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #5  Exercise 2#
+ *	Assignment: Lab #5  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
